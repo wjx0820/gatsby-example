157398f770e0dc507eb3cdb55a1c8d9f516c288b
Jexxie <1989wjx@gmail.com>
2019-06-01 19:05:42 +0800

    feat: add identity support

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,45 +4,50 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Identity from "../components/identity"
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
 
-    <h2>Manual Blogroll</h2>
+      <Identity />
 
-    <ul>
-      <li>
-        <Link to="/blog-html/blog-post-one">Blog Post One</Link>
-      </li>
-      <li>
-        <Link to="/blog-html/blog-post-two">Blog Post Two</Link>
-      </li>
-      <li>
-        <Link to="/blog-html/blog-post-three">Blog Post Three</Link>
-      </li>
-    </ul>
+      <h2>Manual Blogroll</h2>
 
-    <h2>Automatic Blogroll</h2>
-
-    <ul>
-      {data.allMarkdownRemark.edges.map(post => (
-        <li key={post.node.id}>
-          <Link to={post.node.frontmatter.path}>
-            {post.node.frontmatter.title}
-          </Link>
+      <ul>
+        <li>
+          <Link to="/blog-html/blog-post-one">Blog Post One</Link>
         </li>
-      ))}
-    </ul>
+        <li>
+          <Link to="/blog-html/blog-post-two">Blog Post Two</Link>
+        </li>
+        <li>
+          <Link to="/blog-html/blog-post-three">Blog Post Three</Link>
+        </li>
+      </ul>
 
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+      <h2>Automatic Blogroll</h2>
+
+      <ul>
+        {data.allMarkdownRemark.edges.map(post => (
+          <li key={post.node.id}>
+            <Link to={post.node.frontmatter.path}>
+              {post.node.frontmatter.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export const pageQuery = graphql`
   query BlogIndexQuery {
